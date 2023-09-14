a01d8cc7e9ccc0400331deaf157c206e1427d57e
FRIF1 <144664519+FRIF1@users.noreply.github.com>
2023-09-14 16:20:06 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ std::cout<<"Hello World!";
 std::cout<<"Hello World!";
 std::cout<<"Hello World!";
 std::cout<<"Hello World!";
-std::cout<<"Hello World!";
+std::cout<<"Hello World 1111!";
 std::cout<<"Hello World!";
 return 0;
 }
